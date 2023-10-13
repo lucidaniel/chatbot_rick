a83ace9e3eb59197ecb94bd3a6065d6821fc6415
Heinrich Schwalden <lucidcat@localhost.localdomain>
2023-10-13 20:45:08 +0200

    refraction

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import io
 from itertools import cycle
 import datetime
 import json
+
 import requests
 import aiohttp
 import discord
@@ -13,10 +14,11 @@ from discord import Embed, app_commands
 from discord.ext import commands
 from dotenv import load_dotenv
 
-from bot_utilities.ai_utils import generate_response, generate_image_prodia, search, sdxl
+from bot_utilities.ai_utils import generate_response, generate_image_prodia, search, poly_image_gen, generate_gpt4_response, dall_e_gen, sdxl
 from bot_utilities.response_util import split_response, translate_to_en, get_random_prompt
 from bot_utilities.discord_util import check_token, get_discord_token
 from bot_utilities.config_loader import config, load_current_language, load_instructions
+from bot_utilities.replit_detector import detect_replit
 from bot_utilities.sanitization_utils import sanitize_prompt
 from model_enum import Model
 load_dotenv()
@@ -62,12 +64,14 @@ def fetch_chat_models():
     response = requests.get('https://api.naga.ac/v1/models', headers=headers)
     if response.status_code == 200:
         ModelsData = response.json()
-        for model in ModelsData.get('data'):
-            if "chat" in model['endpoints'][0]:
-                models.append(model['id'])
+        models.extend(
+            model['id']
+            for model in ModelsData.get('data')
+            if "chat" in model['endpoints'][0]
+        )
     else:
         print(f"Failed to fetch chat models. Status code: {response.status_code}")
-        
+
     return models
 
 chat_models = fetch_chat_models()
@@ -76,7 +80,7 @@ model_blob = "\n".join(chat_models)
 @bot.event
 async def on_ready():
     await bot.tree.sync()
-    presences_cycle = cycle(presences)
+    presences_cycle = cycle(presences + [current_language['help_footer']])
     print(f"{bot.user} aka {bot.user.name} has connected to Discord!")
     invite_link = discord.utils.oauth_url(
         bot.user.id,
@@ -90,13 +94,12 @@ async def on_ready():
     print(f"\033[1;38;5;46mCurrent model: {config['GPT_MODEL']}\033[0m")
     if presences_disabled:
         return
-    else:
-        while True:
-            presence = next(presences_cycle)
-            presence_with_count = presence.replace("{guild_count}", str(len(bot.guilds)))
-            delay = config['PRESENCES_CHANGE_DELAY']
-            await bot.change_presence(activity=discord.Game(name=presence_with_count))
-            await asyncio.sleep(delay)
+    while True:
+        presence = next(presences_cycle)
+        presence_with_count = presence.replace("{guild_count}", str(len(bot.guilds)))
+        delay = config['PRESENCES_CHANGE_DELAY']
+        await bot.change_presence(activity=discord.Game(name=presence_with_count))
+        await asyncio.sleep(delay)
 
  
 # Set up the instructions
@@ -120,7 +123,7 @@ async def on_message(message):
         if len(replied_messages) > 5:
             oldest_message_id = min(replied_messages.keys())
             del replied_messages[oldest_message_id]
-    
+
     if message.mentions:
         for mention in message.mentions:
             message.content = message.content.replace(f'<@{mention.id}>', f'{mention.display_name}')
@@ -137,7 +140,7 @@ async def on_message(message):
     bot_name_in_message = bot.user.name.lower() in message.content.lower() and smart_mention
     respond_in_thread = isinstance(message.channel, discord.Thread) and (is_bot_mentioned or is_replied or contains_trigger_word)
 
-    if is_active_channel or is_allowed_dm or respond_in_thread or contains_trigger_word or is_bot_mentioned or is_replied or bot_name_in_message:
+    if is_active_channel or is_allowed_dm or respond_in_thread or contains_trigger_word or is_bot_mentioned or is_replied or bot_name_in_message
         if string_channel_id in active_channels:
             instruc_config = active_channels[string_channel_id]          
         else:
@@ -153,7 +156,6 @@ async def on_message(message):
 
         if internet_access:
             instructions += f"""\n\nIt's currently {current_time}, You have real-time information and the ability to browse the internet."""
-        if internet_access:
             await message.add_reaction("🔎")
         channel_id = message.channel.id
         key = f"{message.author.id}-{channel_id}"
@@ -162,9 +164,9 @@ async def on_message(message):
             message_history[key] = []
 
         message_history[key] = message_history[key][-MAX_HISTORY:]
-            
+
         search_results = await search(message.content)
-            
+
         message_history[key].append({"role": "user", "content": message.content})
         history = message_history[key]
 
@@ -235,6 +237,7 @@ async def toggledm(ctx):
     allow_dm = not allow_dm
     await ctx.send(f"DMs are now {'on' if allow_dm else 'off'}", delete_after=3)
 
+
 @bot.hybrid_command(name="toggleactive", description=current_language["toggleactive"])
 @app_commands.choices(persona=[
     app_commands.Choice(name=persona.capitalize(), value=persona)
@@ -249,10 +252,7 @@ async def toggleactive(ctx, persona: app_commands.Choice[str] = instruction[inst
             json.dump(active_channels, f, indent=4)
         await ctx.send(f"{ctx.channel.mention} {current_language['toggleactive_msg_1']}", delete_after=3)
     else:
-        if persona.value:
-            active_channels[channel_id] = persona.value
-        else:
-            active_channels[channel_id] = persona
+        active_channels[channel_id] = persona.value if persona.value else persona
         with open("channels.json", "w", encoding='utf-8') as f:
             json.dump(active_channels, f, indent=4)
         await ctx.send(f"{ctx.channel.mention} {current_language['toggleactive_msg_2']}", delete_after=3)
@@ -267,10 +267,10 @@ async def clear(ctx):
     try:
         message_history[key].clear()
     except Exception as e:
-        await ctx.send(f"⚠️ There is no message history to be cleared", delete_after=2)
+        await ctx.send("⚠️ There is no message history to be cleared", delete_after=2)
         return
-    
-    await ctx.send(f"Message history has been cleared", delete_after=4)
+
+    await ctx.send("Message history has been cleared", delete_after=4)
 
 
 @commands.guild_only()
@@ -315,16 +315,13 @@ async def clear(ctx):
 @commands.guild_only()
 async def imagine(ctx, prompt: str, model: app_commands.Choice[str], sampler: app_commands.Choice[str], negative: str = None, seed: int = None):
     for word in prompt.split():
-        if word in blacklisted_words:
-            is_nsfw = True
-        else:
-            is_nsfw = False
+        is_nsfw = word in blacklisted_words
     if seed is None:
         seed = random.randint(10000, 99999)
     await ctx.defer()
-    
+
     model_uid = Model[model.value].value[0]
-    
+
     if is_nsfw and not ctx.channel.nsfw:
         await ctx.send(f"⚠️ You can create NSFW images in NSFW channels only\n To create NSFW image first create a age ristricted channel ", delete_after=30)
         return
@@ -332,13 +329,13 @@ async def imagine(ctx, prompt: str, model: app_commands.Choice[str], sampler: ap
         imagefileobj = sdxl(prompt)
     else:
         imagefileobj = await generate_image_prodia(prompt, model_uid, sampler.value, seed, negative)
-    
+
     if is_nsfw:
         img_file = discord.File(imagefileobj, filename="image.png", spoiler=True, description=prompt)
         prompt = f"||{prompt}||"
     else:
         img_file = discord.File(imagefileobj, filename="image.png", description=prompt)
-        
+
     if is_nsfw:
         embed = discord.Embed(color=0xFF0000)
     else:
@@ -349,13 +346,72 @@ async def imagine(ctx, prompt: str, model: app_commands.Choice[str], sampler: ap
         embed.add_field(name='📝 Negative Prompt', value=f'- {negative}', inline=False)
     embed.add_field(name='🤖 Model', value=f'- {model.value}', inline=True)
     embed.add_field(name='🧬 Sampler', value=f'- {sampler.value}', inline=True)
-    embed.add_field(name='🌱 Seed', value=f'- {str(seed)}', inline=True)
-    
+    embed.add_field(name='🌱 Seed', value=f'- {seed}', inline=True)
+
     if is_nsfw:
         embed.add_field(name='🔞 NSFW', value=f'- {str(is_nsfw)}', inline=True)
 
     sent_message = await ctx.send(embed=embed, file=img_file)
+
+
+@bot.hybrid_command(name="imagine-dalle", description="Create images using DALL-E")
+@commands.guild_only()
+@app_commands.choices(model=[
+     app_commands.Choice(name='SDXL', value='sdxl'),
+     app_commands.Choice(name='Kandinsky 2.2', value='kandinsky-2.2'),
+     app_commands.Choice(name='Kandinsky 2', value='kandinsky-2'),
+     app_commands.Choice(name='Dall-E', value='dall-e'),
+     app_commands.Choice(name='Stable Diffusion 2.1', value='stable-diffusion-2.1'),
+     app_commands.Choice(name='Stable Diffusion 1.5', value='stable-diffusion-1.5'),
+     app_commands.Choice(name='Deepfloyd', value='deepfloyd-if'),
+     app_commands.Choice(name='Material Diffusion', value='material-diffusion')
+])
+@app_commands.choices(size=[
+     app_commands.Choice(name='🔳 Small', value='256x256'),
+     app_commands.Choice(name='🔳 Medium', value='512x512'),
+     app_commands.Choice(name='🔳 Large', value='1024x1024')
+])
+@app_commands.describe(
+     prompt="Write a amazing prompt for a image",
+     size="Choose the size of the image"
+)
+async def imagine_dalle(ctx, prompt, model: app_commands.Choice[str], size: app_commands.Choice[str], num_images : int = 1):
+    await ctx.defer()
+    model = model.value
+    size = size.value
+    num_images = min(num_images, 4)
+    imagefileobjs = await dall_e_gen(model, prompt, size, num_images)
+    await ctx.send(f'🎨 Generated Image by {ctx.author.name}')
+    for imagefileobj in imagefileobjs:
+        file = discord.File(imagefileobj, filename="image.png", spoiler=True, description=prompt)
+        sent_message =  await ctx.send(file=file)
+        reactions = ["⬆️", "⬇️"]
+        for reaction in reactions:
+            await sent_message.add_reaction(reaction)
+
     
+@commands.guild_only()
+@bot.hybrid_command(name="imagine-pollinations", description="Bring your imagination into reality with pollinations.ai!")
+@app_commands.describe(images="Choose the amount of your image.")
+@app_commands.describe(prompt="Provide a description of your imagination to turn them into image.")
+async def imagine_poly(ctx, *, prompt: str, images: int = 4):
+    await ctx.defer(ephemeral=True)
+    images = min(images, 18)
+    tasks = []
+    async with aiohttp.ClientSession() as session:
+        while len(tasks) < images:
+            task = asyncio.ensure_future(poly_image_gen(session, prompt))
+            tasks.append(task)
+            
+        generated_images = await asyncio.gather(*tasks)
+            
+    files = []
+    for index, image in enumerate(generated_images):
+        file = discord.File(image, filename=f"image_{index+1}.png")
+        files.append(file)
+        
+    await ctx.send(files=files, ephemeral=True)
+
 @commands.guild_only()
 @bot.hybrid_command(name="gif", description=current_language["nekos"])
 @app_commands.choices(category=[
@@ -377,7 +433,17 @@ async def gif(ctx, category: app_commands.Choice[str]):
 
             results = json_data.get("results")
             if not results:
-                await ctx.channel.send("No image found.")
+                await ctx.channel.send("N    string_channel_id = f"{message.channel.id}"
+    is_replied = (message.reference and message.reference.resolved.author == bot.user) and smart_mention
+    is_dm_channel = isinstance(message.channel, discord.DMChannel)
+    is_active_channel = string_channel_id in active_channels
+    is_allowed_dm = allow_dm and is_dm_channel
+    contains_trigger_word = any(word in message.content for word in trigger_words)
+    is_bot_mentioned = bot.user.mentioned_in(message) and smart_mention and not message.mention_everyone
+    bot_name_in_message = bot.user.name.lower() in message.content.lower() and smart_mention
+    respond_in_thread = isinstance(message.channel, discord.Thread) and (is_bot_mentioned or is_replied or contains_trigger_word)
+
+    if is_active_channel or is_allowed_dm or respond_in_thread or contains_trigger_word or is_bot_mentioned or is_replied or bot_name_in_message image found.")
                 return
 
             image_url = results[0].get("url")
@@ -393,5 +459,8 @@ async def on_command_error(ctx, error):
     elif isinstance(error, commands.NotOwner):
         await ctx.send(f"{ctx.author.mention} Only the owner of the bot can use this command.")
 
+if detect_replit():
+    from bot_utilities.replit_flask_runner import run_flask_in_thread
+    run_flask_in_thread()
 if __name__ == "__main__":
     bot.run(TOKEN)
